01d0c7be8a7d4afa1c856914784abdd34f9630ac
Maxim Piskunov <maxitg@neutrino.local>
2013-03-24 19:57:51 +0400

    Initial Commit

---
+++ Gamma Rays/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Gamma Rays
+//
+//  Created by Maxim Piskunov on 24.03.2013.
+//  Copyright (c) 2013 Maxim Piskunov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
